dc1b4c8faae07f069d4dbbd16e295c801b285037
Andreas Kromke <Andreas.Kromke@gmail.com>
2025-12-28 12:11:36 +0100

    register MIME type for Atari programs and assign them to the emulator program

---
+++ install-mime-types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script registers Atari executable files with a MIME type and a respective icon.
+# Must be run with sudo prefix.
+xdg-mime install --novendor --mode system assets/atariprogram.xml
+xdg-icon-resource install --context mimetypes --size 512 assets/atariprogram.png atariprogram
+update-mime-database -V /usr/share/mime
+update-icon-caches /usr/local/share/icons
+update-desktop-database /usr/local/share/applications
+
+#xdg-mime uninstall --novendor --mode system atariprogram.xml

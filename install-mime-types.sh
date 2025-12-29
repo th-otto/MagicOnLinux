689eb10e43ec4a9851861ec91f5f0c409458a6d3
Andreas Kromke <Andreas.Kromke@gmail.com>
2025-12-29 22:11:12 +0100

    optionally register st disk images or add missing icon

--- install-mime-types.sh
+++ install-mime-types.sh
@@ -8,3 +8,16 @@ update-icon-caches /usr/local/share/icons
 update-desktop-database /usr/local/share/applications
 
 #xdg-mime uninstall --novendor --mode system assets/atariprogram.xml
+
+# if Hatari is installed, then add the following line:
+#    <generic-icon name="application-x-st-disk-image"/>
+# in
+#    /usr/local/share/mime/packages/hatari.xml
+# after
+#    <glob pattern="*.st"/>
+#
+# otherwise:
+#xdg-mime install --novendor --mode system assets/st-disk-image.xml
+#xdg-icon-resource install --context mimetypes assets/application-x-st-disk-image.svg application-x-st-disk-image
+#update-mime-database -V /usr/share/mime
+#update-icon-caches /usr/local/share/icons

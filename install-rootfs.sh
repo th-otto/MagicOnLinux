74c04675df2e6062f4b7844b04de03cccbf7a74d
Andreas Kromke <Andreas.Kromke@gmail.com>
2025-12-31 16:22:48 +0100

    rootfs installation to intended directory

--- install-rootfs.sh
+++ install-rootfs.sh
@@ -5,7 +5,7 @@ INITIAL_LANGUAGE=EN
 
 echo "create Atari root filesystem C: in $ROOTFS  ..."
 mkdir -p $ROOTFS
-cp -rpn rootfs $ROOTFS
+cp -rpn rootfs/* $ROOTFS/
 echo "localise Atari root filesystem to $INITIAL_LANGUAGE ..."
 touch $ROOTFS/MAGICLIN.OS
 $ROOTFS/LANG/LOCALISE.SH $INITIAL_LANGUAGE

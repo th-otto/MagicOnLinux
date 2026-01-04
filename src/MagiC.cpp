b046ceff5610e11a356a5c7469f68bf0cf5c42c5
Andreas Kromke <Andreas.Kromke@gmail.com>
2026-01-04 12:31:09 +0100

    Atari cold boot exits emulator

--- src/MagiC.cpp
+++ src/MagiC.cpp
@@ -3211,8 +3211,8 @@ uint32_t CMagiC::AtariColdBoot(uint32_t params, uint8_t *addrOffset68k)
 {
     (void) params;
     (void) addrOffset68k;
-    DebugError2("() - yet to be implemented");
-    return 0;
+    DebugError2("() - yet to be implemented. Instead exit application.");
+    return AtariExit(params, addrOffset68k);
 }
 
 
